cf809395e6f8490b496152234f19ee036901521e
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-02-20 14:04:48 +0530

    Added:Linked-list program

---
+++ src/com/bridgelabz/linkedlist/LinkedListProblem.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedListProblem {
+    public static void main(String[] args) {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyNode<Integer> myFourthNode = new MyNode<>(40);
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(myFirstNode,mySecondNode);
+        myLinkedList.search(mySecondNode);
+        myLinkedList.insertUsingKey(mySecondNode,myFourthNode);
+        myLinkedList.deleteKey(myFourthNode);
+        myLinkedList.printMyNodes();
+        myLinkedList.size();
+
+
+    }
+
+}
