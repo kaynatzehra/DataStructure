fa6b021af8f9ff9ef8c54d0ed7e5d9a96f7edbbb
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-02-20 19:07:59 +0530

    Added:Linked-list by adding

--- src/com/bridgelabz/linkedlist/INode.java
+++ src/com/bridgelabz/linkedlist/INode.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.linkedlist;
 
-public interface INode<K extends Comparable> {
+public interface INode<K> {
     K getKey();
+
     void setKey();
 
     INode getNext();
+
     void setNext(INode tempNode);
 }
--- src/com/bridgelabz/linkedlist/LinkedListProblem.java
+++ src/com/bridgelabz/linkedlist/LinkedListProblem.java
@@ -5,19 +5,11 @@ public class LinkedListProblem {
         MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myThirdNode = new MyNode<>(70);
-        MyNode<Integer> myFourthNode = new MyNode<>(40);
-
         MyLinkedList myLinkedList = new MyLinkedList();
         myLinkedList.add(myFirstNode);
-        myLinkedList.append(myThirdNode);
-        myLinkedList.insert(myFirstNode,mySecondNode);
-        myLinkedList.search(mySecondNode);
-        myLinkedList.insertUsingKey(mySecondNode,myFourthNode);
-        myLinkedList.deleteKey(myFourthNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
         myLinkedList.printMyNodes();
-        myLinkedList.size();
-
-
     }
 
 }
